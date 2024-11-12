21f8266752df5abd9d09fd1d07e069512d7b43ec
Jolan <jolan.wagner@gmail.com>
2024-11-12 18:47:16 +0100

    Piscine 42 -> C04. ✅

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jowagner <jowagner@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 18:39:39 by jowagner          #+#    #+#             */
+/*   Updated: 2024/09/27 11:46:05 by jowagner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+	}
+	ft_putchar(nb % 10 + '0');
+}
+
+/* #include <stdlib.h>
+
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+		return (0);
+	ft_putnbr(atoi(av[1]));
+} */
---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jowagner <jowagner@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 20:04:48 by jowagner          #+#    #+#             */
+/*   Updated: 2024/09/27 11:47:38 by jowagner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (sign * result);
+}
+
+/* #include <stdio.h>
+#include <stdlib.h>
+
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+		return (0);
+	printf("%d\n", ft_atoi(av[1]));
+	printf("%d", atoi(av[1]));
+} */
